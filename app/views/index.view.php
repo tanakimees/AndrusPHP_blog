5ed0ac5d77ab43573579b6452e90b9e76e31e480
Kristo Tänak <tanak.kristo@gmail.com>
2023-02-20 15:25:16 +0200

    xdxdxd

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -1,5 +1,11 @@
-<?php require_once('partials/head.php'); ?>
+<?php
+require_once('partials/head.php');
+require_once('core/database/Connection.php');
+require_once('app/controllers/ProjectsController.php');
+?>
 
 <h1>Home Page</h1>
+
+<h1><?=$blogi?></h1>
 
 <?php require_once('partials/footer.php'); ?>
